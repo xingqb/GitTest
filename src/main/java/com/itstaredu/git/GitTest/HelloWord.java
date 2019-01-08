e1304d508272562da6ca869eb279898e98b3c467
hunter_glb <goodMorning_glb@163.com>
2019-01-08 22:30:59 +0800

    second message

---
+++ src/main/java/com/itstaredu/git/GitTest/HelloWord.java
@@ -0,0 +1,11 @@
+package com.itstaredu.git.GitTest;
+/**
+ * @author Hunter
+ * @date 2019年1月8日 下午10:30:14
+ * @version 1.0
+ */
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("nihao");
+	}
+}
